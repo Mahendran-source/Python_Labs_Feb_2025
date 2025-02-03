65d46f1f2e7bdea5668a6c52c36c032c13c54e4c
Mahendran-source <moorthy.mahendran@gmail.com>
2025-02-03 22:52:56 +0000

    Added first.py

---
+++ first.py
@@ -0,0 +1,17 @@
+#! /usr/binenv python3
+# Author: Mahendran
+# Version: 1.0
+# Description: This is our first script
+
+#name ="Mahendran Moorthy"
+
+name =input("Enter your name :")
+print("My name is", name)
+print("My nme is " + name)
+
+import random
+print("Lucky number is", random.randint(1,50))
+
+import math
+print("Cosine of 0.5 is", math.cos(0.5))
+
