2ba637aca997792c39888bde3b1a5350abf90cc5
Mahendran-source <moorthy.mahendran@gmail.com>
2025-02-04 20:21:02 +0000

    Added demo_counter_loops.py

---
+++ demo_counter_loops.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+# Author: Mahendran
+# Description:  This script will demo HOWTO repeat a block of
+# commands a specific number of times using a COUNTER loop.
+"""
+      Docstring:
+"""
+
+count = 0 # 1. Initialise counter
+while count < 10: # 2. Test counter
+    print(count)
+    count += 1
+
+# Alternatively, we could use a FOR loop pluse the build-in
+# range(start, stop, step) function.
+for num in range(0, 10, 1):
+    print("Num value: ", num)
+
+# range(start, stop, step=1) function
+for num in range(0,10)
+
+# range (start=0,stop, step=1)
+for num in range(10)
+    print(num)
+
+
+
+
+
+
+
