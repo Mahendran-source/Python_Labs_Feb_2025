e88a2dddf6bac98de6b753a22c23ec4030593caf
Mahendran-source <moorthy.mahendran@gmail.com>
2025-02-04 01:21:07 +0000

    Added ex1.py & ex2.py

---
+++ ex1.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+# Author: Mahendran
+# Version: 1
+# Description: Example script to print first and last name
+
+first = input("Enter your first name :")
+last  = input("Enter your last name :")
+print("Your name is :", first +" "+last)
---
+++ ex2.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python 3
+# Author : Mahendran
+# Version: 1
+# Description: Working with strings
+
+first = 'Mahendran'
+last = 'Moorthy'
+
+print(first,last)
+
+# now transfer these variables into a list
+
+names =[first,last]
+
+print(names)
+
+# Transfer these variables values into a dictionary
+
+mydict ={'first':first,'last':last}
+
+print(mydict['first'],mydict['last'])
+
