5b83d07b38b75711b562d500cb1c347030b31319
Mahendran-source <moorthy.mahendran@gmail.com>
2025-02-04 19:47:47 +0000

    Added demo_pin.py

--- demo_pin.py
+++ demo_pin.py
@@ -15,8 +15,13 @@ while pin != master_pin and attempts < 3:
     pin = input("Enter PIN: ")
     if pin == master_pin:
         print("Valid PIN")
+        break # Break the entire while loop including else.
     else:
         print("Ïnvalid PIN")
         attempts += 1
+else:
+    # Execute only once when loop becomes false
+    print("Too many attempts", attempts)
+    print("Your card has been retained. Have a nice day!")
 
 print("Done.")
