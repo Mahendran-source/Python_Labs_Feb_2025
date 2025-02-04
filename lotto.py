51b9127fc192f809c8abdd057b7bf670e8b1bf88
Mahendran-source <moorthy.mahendran@gmail.com>
2025-02-04 18:45:31 +0000

    Added lotto.py

---
+++ lotto.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+# Author: Mahendran
+# Description:  This script will demo
+"""
+      Docstring:
+"""
+import random
+
+lotto = []
+
+while len(lotto) < 6:
+    num = random.randint(1,50)
+    if num not in lotto:
+        lotto.append(num)
+    else:
+        print("Duplicate number =", num)
+
+print("Lottery numbers =", lotto)
