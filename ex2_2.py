35cb71dc08a8dbe40211577500b851d60841c1b6
Mahendran-source <moorthy.mahendran@gmail.com>
2025-02-04 01:33:32 +0000

    Added ex2_2.py

---
+++ ex2_2.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python 3
+# Author: Mahendran
+# Version: 1
+# Description: This is test string functions.
+
+var = input("Plese enter a value: ")
+
+# Display the value in upper case
+print(var.upper())
+
+# Display the number of character
+print(len(var))
