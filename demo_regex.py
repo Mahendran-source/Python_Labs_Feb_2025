5b5fb0493543136d650d5345a4f54741f25cc08d
Mahendran-source <moorthy.mahendran@gmail.com>
2025-02-05 19:18:25 +0000

    updated demo_regex.py

--- demo_regex.py
+++ demo_regex.py
@@ -14,7 +14,14 @@ for line in fh_in:
     # m = re.search("^the", line) # Match lines starting with 'the'
     # m = re.search("ing$", line) # Match lines ending with 'ing'
     # m = re.search("^ring$", line) # Match lines with 'ring'
-    m = re.search("^ring$", line)
+    # m = re.search("^.ing$", line) # Match lines with with 4 char ending with 'ing'
+    # m = re.search("^[adr]ing$", line) # Match lines with [adr] followed 'ing'
+    # m = re.search("[aeiou][aeiou][aeiou]", line) # Match three continues vowls in a line.
+    # m = re.search("[.]", line) # Match lines with a DOT.
+    # m = re.search("^[A-Z].*[A-Z]$", line) # Match lines start/end with a Capital
+    # m = re.search("^[A-Z].{4}[A-Z]$", line) # Match lines of 6 char start/end with a Capital
+    # m = re.search(r"^(.)(.).(\2)(\1)$", line) # Match with palindromes
+    m = re.search(r"^([A-Z]).*(\1)$", line) # Match start and ending with same letter.
     if m:
         print(line,end="")
 
